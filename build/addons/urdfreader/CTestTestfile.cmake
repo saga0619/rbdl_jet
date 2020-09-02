792ca1d0600305cdc08be6ad6a592e88a05e87d9
seungbin <ysb127@snu.ac.kr>
2020-09-02 13:57:42 +0900

    delete build

--- build/addons/urdfreader/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/seungbin/Downloads/rbdl-rbdl-849d2aee8f4c/addons/urdfreader
-# Build directory: /home/seungbin/Downloads/rbdl-rbdl-849d2aee8f4c/build/addons/urdfreader
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../../gtest")
--- build/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest
-# Build directory: /home/seungbin/Downloads/rbdl-rbdl-849d2aee8f4c/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
